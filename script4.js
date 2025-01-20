3bda6493e92e7c590e55e00a56d1666c9ec1bd42
ellilbbh <elisabethkoeksal@icloud.com>
2025-01-20 13:14:31 +0100

    .

---
+++ script4.js
@@ -0,0 +1,23 @@
+let currentIndex = 0;
+
+function showSlide(index) {
+    const slides = document.querySelectorAll('.slide');
+    const totalSlides = slides.length;
+
+    if (index >= totalSlides) currentIndex = 0;
+    else if (index < 0) currentIndex = totalSlides - 1;
+    else currentIndex = index;
+
+    document.querySelector('.carousel-container').style.transform = `translateX(${-currentIndex * 100}%)`;
+}
+
+function nextSlide() {
+    showSlide(currentIndex + 1);
+}
+
+function prevSlide() {
+    showSlide(currentIndex - 1);
+}
+
+// Automatischer Bildwechsel alle 4 Sekunden
+setInterval(nextSlide, 4000);
